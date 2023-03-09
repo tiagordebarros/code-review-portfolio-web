e15b6c615b07a89434b3011f498c7ba48936aab4
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-03-09 13:33:17 -0300

    refactor: remove nodemailer basic config

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
+
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const nodemailer = require("nodemailer");
 const app = express();
 const sibApi = require("sib-api-v3-sdk");
+
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
@@ -32,49 +33,6 @@ app.post("/subscribe", (req, res) => {
       return res.status(500).json(error);
     }
   );
-});
-
-const contactEmail = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  auth: {
-    user: process.env.EMAIL_ADDRESS,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Serviço de e-mails iniciado com sucesso!");
-  }
-});
-
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: process.env.EMAIL_ADDRESS,
-    subject: "Contato via Formuário - Portfólio Web",
-    html: `<p>Nome: ${name}</p>
-           <p>E-mail: ${email}</p>
-           <p>Telefone: ${phone}</p>
-           <p>Mensagem: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Mensagem Enviada" });
-    }
-  });
 });
 
 app.use((err, _req, res, _next) => {
