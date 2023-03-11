989481abd63f25a90fc503a5c0e05efbb7eca86b
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-03-11 20:48:19 -0300

    chore: add Spacefile config

--- personal-portfolio/backend/api/server.js
+++ personal-portfolio/backend/api/server.js
@@ -4,4 +4,4 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log("Servidor inicializado com sucesso!"));
+app.listen(port, () => console.log(`Servidor escutando na porta ${port}!`));
